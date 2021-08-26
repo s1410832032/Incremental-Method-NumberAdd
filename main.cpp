55ab8880b9cb6f9bcac529270c5502d24adc8e46
Yumi Chen <66152558+s1410832032@users.noreply.github.com>
2021-08-26 09:36:13 +0000

    copyteat0826

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+using namespace std;
+
+void copy(char bef[]){
+  int j=0;
+  for (int x=0;bef[x] !=0;x++)
+    j++;
+  int i =0;
+  char aft[j];
+  for(i=0;bef[i] != '\0';i++)
+    aft[i]=bef[i];
+  aft[i]='\0';
+  cout << "finish copy:" << aft;
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+  char bef[15] = "incremental";
+  char aft[15];
+  int i=0;
+
+  for (i=0 ; bef[i] != '\0' ; i++)
+    aft[i]=bef[i];
+  aft[i]='\0';
+
+  cout << "Sentence to copy: " << bef << "\n";
+  cout << "the result:" << aft <<"\n";
+
+  copy(bef);
+
+return 0;
 }
